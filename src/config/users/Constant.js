9e0bfe2375336268a95b1f5a51ab7a33f48b3be3
greencoconutfield <magnolia00009@gmail.com>
2023-02-24 13:42:21 +0700

    Update Constant.js

--- src/config/users/Constant.js
+++ src/config/users/Constant.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-       hostname: 'http://ec2-44-230-36-154.us-west-2.compute.amazonaws.com'
-    //   hostname: 'http://localhost'
+//        hostname: 'http://ec2-44-230-36-154.us-west-2.compute.amazonaws.com'
+       hostname: 'http://localhost'
   }
   export default CONFIG
